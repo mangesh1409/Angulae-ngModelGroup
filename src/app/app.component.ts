2495c382a1b748426cfff6013e2d81b6960caf75
Mangesh Jadhav <149mpj@gmail.com>
2020-07-21 00:45:29 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'TemplateFrom';
+  default="Angular";
+  gender:any=[  { id:1,val:"Male" },{ id:2,val:"Female" } ]
+
+  /* Acess Element using @ViewChild  */
+  /*@ViewChild('form') myForm:NgForm;
+  onSubmit(){
+    console.log(this.myForm);
+  }*/
+
+  onSubmit(form:NgForm){
+    // console.log(form.value);
+    console.log(form.value);
+  }
 }
